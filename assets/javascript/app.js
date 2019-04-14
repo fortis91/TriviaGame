d4cec7a17af1572219f5b675e55ddd27ca087220
Nigel Phillips <nigel@Nigels-iMac.lan>
2019-04-14 13:46:18 -0500

    frontend layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,5 @@
-$(document).ready(function() {
+$(document).ready(function () {
     console.log("ready to go");
-    // var options = [{
-    //         question: "Capital of Illinois",
-    //         choice: ["Springfield", "Chicago", "Blue Island", "Harvey"],
-    //         answer: 0,
-    //         photo: "assets/images/herring.jpg"
-    //     },
-    //     {
-    //         question: "Capital of Jamaica",
-    //         choice: ["Bronx", "Kingston", "Spanish Town", "St Ann"],
-    //         answer: 1,
-    //         photo: "assets/images/lemon.gif"
-    //     },
-    //     {
-    //         question: "Capital of England?",
-    //         choice: ["Bristol", "Livepool", "London", "Jamaica"],
-    //         answer: 2,
-    //         photo: "assets/images/guava.gif"
-    //     }
-    // ];
 
     var correctCount = 0;
     var wrongCount = 0;
@@ -26,69 +7,71 @@ $(document).ready(function() {
     var timer = 5;
     var intervalId;
     var userGuess = "";
-    // var running = false;
     var qCount = options.length;
     var pick;
     var index;
-    var newArray = [];
     var alreadyAsked = [];
 
     //------------------------------------------------------------------------//    
-    var init = function() {
-            console.clear();
-            console.log("initialize");
-            $("#start").hide();
-            displayQuestion();
-            timerO.start();
-            for (var i = 0; i < options.length; i++) {
-                alreadyAsked.push(options[i]);
-            }
+    var init = function () {
+        console.clear();
+        console.log("initialize");
+        //$("#start").hide();
+        //displayQuestion();
+        timerO.start();
+        for (var i = 0; i < options.length; i++) {
+            alreadyAsked.push(options[i]);
         }
-        //------------------------------------------------------------------------//    
+    }
+    //------------------------------------------------------------------------//    
 
     $("#reset").hide();
 
-    $("#start").on("click", function() {
+    $("#start").on("click", function () {
         console.log("starting game");
         $("#start").hide();
         displayQuestion();
-        startTimer();
+        timerO.start();
         for (var i = 0; i < options.length; i++) {
             alreadyAsked.push(options[i]);
         }
     })
 
+
+
+      
+
     //----------------------OOP--------------------------//
     //todo convert to class - https://medium.com/javascript-scene/javascript-factory-functions-vs-constructor-functions-vs-classes-2f22ceddf33e
     var timerO = {
-            running: false,
-            start: function() {
-                console.log("timerO: start timer");
-                if (!this.running) {
-                    intervalId = setInterval(this.decrement, 1000);
-                    this.running = true;
-                }
-            },
-            decrement: function() {
-                console.log("timerO: decrement: " + timer);
-                // $("#timeleft").html("<h3>Time remaining: " + timer + "</h3>");
-                $("#timeleft").html("Time remaining: " + timer);
-                timer--;
-
-                if (timer === 0) {
-                    unanswerCount++;
-                    timerO.stop();
-                    $("#answerblock").html("<p>Time is up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
-                    displayResult();
-                }
-            },
-            stop: function() {
-                console.log("timerO: stop timer");
-                this.running = false;
-                clearInterval(intervalId);
+        running: false,
+        start: function () {
+            console.log("timerO: start timer");
+            if (!this.running) {
+                intervalId = setInterval(this.decrement, 1000);
+                this.running = true;
             }
+        },
+        decrement: function () {
+            // console.log("timerO: decrement: " + timer);
+            // $("#timeleft").html("<h3>Time remaining: " + timer + "</h3>");
+            $("#timeleft").html("Time remaining: " + timer);
+            timer--;
+
+            if (timer === 0) {
+                unanswerCount++;
+                timerO.stop();
+                $("#answerblock").html("<p>Time is up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+                displayResult();
+            }
+        },
+        stop: function () {
+            console.log("timerO: stop timer");
+            this.running = false;
+            clearInterval(intervalId);
         }
-        //----------------------OOP--------------------------//    
+    }
+    //----------------------OOP--------------------------//    
 
     function displayQuestion() {
         console.log("display question");
@@ -96,23 +79,43 @@ $(document).ready(function() {
 
         $("#questionblock").html("<h3>" + pick.question + "</h3>");
         for (var i = 0; i < pick.choice.length; i++) {
-            var userChoice = $("<div>");
-            userChoice.addClass("answerchoice");
-            userChoice.html(pick.choice[i]);
-            //assign array position to it so can check answer
-            userChoice.attr("data-guessvalue", i);
-            $("#answerblock").append(userChoice);
+            var newDiv = $('<div class="form-check">');
+            var radioButton = $('<input class="form-check-input" type="radio" name="radio" id="' + i + '" value="' + i + '" />');
+            var radioLabel = $('<label class="form-check-label" for="' + i + '">').text(pick.choice[i]);
+            newDiv.append(radioButton, radioLabel);
+            $('#answerblock').append(newDiv);
         }
 
-        $(".answerchoice").on("click", function() {
+        $(".form-check-input").on("click", function () {
             console.log("click answer: ");
-            //grab array position from userGuess
-            userGuess = parseInt($(this).attr("data-guessvalue"));
+            userGuess = parseInt(this.value);
             console.log("user guess: " + userGuess + "/" + "answer: " + pick.answer);
             checkAnswer(userGuess, pick);
 
         })
     }
+
+    // function displayQuestion() {
+    //     console.log("display question");
+    //     pick = options[generateRandomNumnber(options.length)];
+
+    //     $("#questionblock").html("<h3>" + pick.question + "</h3>");
+    //     for (var i = 0; i < pick.choice.length; i++) {
+    //         var userChoice = $("<div>");
+    //         userChoice.addClass("answerchoice");
+    //         userChoice.html(pick.choice[i]);
+    //         userChoice.attr("data-guessvalue", i);
+    //         $("#answerblock").append(userChoice);
+    //     }
+
+    //     $(".answerchoice").on("click", function () {
+    //         console.log("click answer: ");
+    //         userGuess = parseInt($(this).attr("data-guessvalue"));
+    //         console.log("user guess: " + userGuess + "/" + "answer: " + pick.answer);
+    //         checkAnswer(userGuess, pick);
+
+    //     })
+    // }
 
     function checkAnswer(userGuess, pick) {
         console.log("checkAnswer - pick: " + pick.answer);
@@ -133,13 +136,13 @@ $(document).ready(function() {
         // $("#answerblock").append("<img src=" + pick.photo + ">");
         options.splice(index, 1);
 
-        setTimeout(function() {
+        setTimeout(function () {
             $("#answerblock").empty();
             timer = 5;
 
             //run the score screen if all questions answered
             if ((wrongCount + correctCount + unanswerCount) === qCount) {
-                console.log(options.length + "/" + alreadyAsked.length);
+                // console.log(options.length + "/" + alreadyAsked.length);
                 //todo extract to method
                 $("#questionblock").empty();
                 $("#questionblock").html("<h4>Game Over: </h4>");
@@ -152,15 +155,14 @@ $(document).ready(function() {
                 unanswerCount = 0;
 
             } else {
-                console.log(options.length + "/" + alreadyAsked.length);
+                // console.log(options.length + "/" + alreadyAsked.length);
                 timerO.start();
                 displayQuestion();
-
             }
         }, 3000);
     }
 
-    $("#reset").on("click", function() {
+    $("#reset").on("click", function () {
         console.log("reset game");
         $("#reset").hide();
         $("#answerblock").empty();
@@ -172,20 +174,27 @@ $(document).ready(function() {
         displayQuestion();
     })
 
-    var generateRandomNumnber = function(max) {
+    var generateRandomNumnber = function (max) {
         return Math.floor(Math.random() * max);
     }
 
-    var getNextQuestion = function() {
-        var alreadyUsed = [];
-        var index = generateRandomNumnber(options.length);
+    var getNextQuestion = function () {
+        var randomNumber = generateRandomNumnber(options.length);
+        // console.log("random: " + randomNumber +" more questions: "+moreQuestions);
         if (alreadyAsked.length === options.length) {
-            console.log("done");
+            console.log("asked filled");
+            alreadyAsked = [];
+            moreQuestions = false;
         }
-        var question = options[index];
-        // if ()
-
+        var question = options[randomNumber];
+        if (moreQuestions && alreadyAsked.indexOf(question) !== -1) {
+            console.log("recursive");
+            return getNextQuestion();
+        }
+        alreadyAsked.push(question);
+        return question;
     }
 
-    init();
+    //init();
+    //displayQuestion();
 })
